1b98bfc82e67575b7133ce5c1046442cb53304a4
Daria Shaieva <sana22_shdo@student.ztu.edu.ua>
2022-11-12 11:52:48 +0200

    changing the text of the first three lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 1");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 2");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
