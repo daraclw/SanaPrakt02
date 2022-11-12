fceb7b65f225c7cd5e364dd2564f4374b3c223ac
Daria Shaieva <sana22_shdo@student.ztu.edu.ua>
2022-11-12 11:53:32 +0200

    changing the text of the last two lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -3,5 +3,5 @@
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 1");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 2");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 3");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 4");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp 5");
